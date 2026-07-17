638f2d5e0dab4309f893e05ed3eb7287313c4069
Gustavo <pow10@txstate.edu>
2026-07-17 15:49:00 -0500

    added functionality of buttons to display card values

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById('message-el')
+// let sumEl = document.getElementById('sum-el')
+let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.getElementById('cards-el')
 
 function startGame() {
+    cardsEl.textContent += " " + firstCard + ", " + secondCard 
+    sumEl.textContent += " " + sum
     if (sum < 21) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -16,6 +22,10 @@ function startGame() {
         isAlive = false
     }
 
-    console.log(message)
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let thirdcard = 7
 }
 

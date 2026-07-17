8fada2d06168b8e85dc0603e32e40359f216ab5c
Gustavo <pow10@txstate.edu>
2026-07-16 21:52:58 -0500

    implement game start button and startGame function

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo!! You got BlackJack"
-    hasBlackJack = true
-} else if (sum > 21) {
-    message = "You're out of the game!"
-    isAlive = false
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo!! You got BlackJack"
+        hasBlackJack = true
+    } else if (sum > 21) {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    console.log(message)
 }
 
-console.log(message)
